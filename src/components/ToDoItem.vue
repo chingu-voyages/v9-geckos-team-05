8f9b19c1335277e5e819c805976518a07014265f
lakrids-mint <mettepernille@hotmail.com>
2019-06-09 19:38:43 +0200

    initial setup

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,10 @@
+<template>
+  <li>hey</li>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="todo">
+    <h1>Do this!</h1>
+    <button>New Todo</button>
+    <input type="text" v-model="todo" @click.prevent="addTodo">
+    <ul v-for="todo in todos">
+      <li>{{todo}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import ToDoItem from "@/components/ToDoItem.vue";
+
+export default {
+  data() {
+    return {
+      todos: ["nachos", "cars"],
+      todo: ""
+    };
+  },
+  components: {
+    ToDoItem
+  },
+  methods: {
+    addTodo: () => {
+      //take user input and add to list->
+
+      console.log(this.todos);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
